4df2a8cb734f18fd8a08619bdc707293588d34a7
BunnyHatcher <martinengelhaus@gmx.de>
2023-01-04 11:47:36 +0100

    minor changes

--- RedHood/Assets/Scripts/RedHood/StateMachine/RH_TARGETING.cs
+++ RedHood/Assets/Scripts/RedHood/StateMachine/RH_TARGETING.cs
@@ -16,7 +16,7 @@ public class RH_TARGETING : RH_BaseState
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log(_targeter.CurrentTarget.name);
+        //Debug.Log(_targeter.CurrentTarget.name);
 
         /*
         if (_targeter.CurrentTarget == null)
@@ -33,7 +33,7 @@ public class RH_TARGETING : RH_BaseState
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        _targeter.Cancel();
+       // _targeter.Cancel();
     }
 
 
